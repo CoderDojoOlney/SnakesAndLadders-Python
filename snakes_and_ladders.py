96d73cfedb190599c1ad17690e71c73d02daa81a
Tim Greaves <tim@greavest.co.uk>
2019-05-19 21:16:01 +0100

    Refactored the ladder code.

--- snakes_and_ladders.py
+++ snakes_and_ladders.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 TILE_SIZE = 80
 PLAYER_SIZE = 20
@@ -7,14 +8,20 @@ CELLS_PER_COLUMN = 10
 
 class Tile(pygame.sprite.Sprite):
 
-    def __init__(self, x, y, colour):
+    def __init__(self, x, y, column, row, colour):
         pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.Surface((TILE_SIZE, TILE_SIZE))
         self.image.fill(colour)
 
         self.rect = self.image.get_rect()
+        pygame.draw.rect(self.image, pygame.Color(0, 0, 0), self.rect, 1)
+
         self.rect.topleft = (x, y)
+
+        self.column = column
+        self.row = row
+        self.index = 0
 
 class PlayerPiece(pygame.sprite.Sprite):
 
@@ -31,23 +38,32 @@ class PlayerPiece(pygame.sprite.Sprite):
 
 class Ladder(pygame.sprite.Sprite):
 
-    def __init__(self, columns, rows):
+    def __init__(self, entry_tile, exit_tile):
         pygame.sprite.Sprite.__init__(self)
+
+        self.entry_index = entry_tile.index
+        self.exit_index = exit_tile.index
+
+        rows = abs(entry_tile.row - exit_tile.row) + 1
+        columns = abs(entry_tile.column - exit_tile.column) + 1
 
         self.image = pygame.Surface((TILE_SIZE * columns, TILE_SIZE * rows), pygame.SRCALPHA)
         self.rect = self.image.get_rect()
-        pygame.draw.line(self.image, pygame.Color(0, 255, 255), self.rect.bottomleft, self.rect.topright, 5)
+        
+        top = min(entry_tile.rect.top, exit_tile.rect.top)
+        left = min(entry_tile.rect.left, exit_tile.rect.left)
+        self.rect.topleft = (left, top)
 
-        self.entry_index = -1
-        self.exit_index = -1
+        start_center = Ladder.calculate_relative_point(entry_tile.rect.center, self.rect.topleft)
+        end_center = Ladder.calculate_relative_point(exit_tile.rect.center, self.rect.topleft)
+        pygame.draw.line(self.image, pygame.Color(0, 255, 0), start_center, end_center, 5)
+        pygame.draw.circle(self.image, pygame.Color(0, 255, 0), start_center, 10)
+        pygame.draw.circle(self.image, pygame.Color(255, 0, 0), end_center, 10)
 
-    def set_position(self, top_left_tile, entry_index, exit_index):
-        self.rect.topleft = top_left_tile.rect.topleft
-        self.entry_index = entry_index
-        self.exit_index = exit_index
+    def calculate_relative_point(point, origin):
+        return (point[0] - origin[0], point[1] - origin[1])
 
 class MainGame():
-
 
     def __init__(self):
         pygame.init()
@@ -60,20 +76,24 @@ class MainGame():
             player.set_position(self.grid_cells[0])
 
     def initialise_grid(self):
-        colors = [pygame.Color(255, 0, 0), pygame.Color(0, 255, 0), pygame.Color(0, 0, 255)]
+        colors = [pygame.Color(255, 200, 200), pygame.Color(200, 255, 200), pygame.Color(200, 200, 255)]
         group = []
         color_index = 0
-        for y in range(0, TILE_SIZE * CELLS_PER_ROW, TILE_SIZE):
-            for x in range(0, TILE_SIZE * CELLS_PER_COLUMN, TILE_SIZE):
-                group.append(Tile(x, y, colors[color_index % len(colors)]))
+        for row in range(0, CELLS_PER_COLUMN):
+            for column in range(0, CELLS_PER_ROW):
+                group.append(Tile(column * TILE_SIZE, row * TILE_SIZE, column, row, colors[color_index % len(colors)]))
                 color_index += 1
         ordered_group = []
-        for y in range(CELLS_PER_ROW - 1, -1, -1):
-            rng = range(0, CELLS_PER_COLUMN)
-            if y % 2 == 0:
+        current_index = 0
+        for row in range(CELLS_PER_COLUMN - 1, -1, -1):
+            rng = range(0, CELLS_PER_ROW)
+            if row % 2 == 0:
                 rng = reversed(rng)
-            for x in rng:
-                ordered_group.append(group[y * CELLS_PER_ROW + x])
+            for column in rng:
+                this_tile = group[row * CELLS_PER_ROW + column]
+                this_tile.index = current_index
+                ordered_group.append(this_tile)
+                current_index += 1
 
         return ordered_group
 
@@ -84,8 +104,10 @@ class MainGame():
 
     def initialise_obstacles(self):
         obstacles = []
-        obstacles.append(Ladder(2,3))
-        obstacles[0].set_position(self.grid_cells[25], 5, 26)
+        obstacles.append(Ladder(self.grid_cells[3], self.grid_cells[25]))
+        obstacles.append(Ladder(self.grid_cells[12], self.grid_cells[43]))
+        obstacles.append(Ladder(self.grid_cells[34], self.grid_cells[72]))
+        obstacles.append(Ladder(self.grid_cells[66], self.grid_cells[96]))
         return obstacles
 
     def move_player_steps(self, player, steps):
@@ -107,6 +129,7 @@ class MainGame():
             for o in self.obstacles:
                 if self.players[0].tile_index == o.entry_index:
                     self.players[0].tile_index = o.exit_index
+                    self.move_player_steps(self.players[0], 0)
 
 if __name__ == "__main__":
     game = MainGame()
